38c28d23a8801ade568a16ebd86875c121769a26
Cory Barr <cory.barr@gmail.com>
2024-06-17 17:18:02 -0700

    add KHR_texture_transform

---
+++ CesiumIonRevitAddin/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace CesiumIonRevitAddin
+{
+    internal class Logger
+    {
+        static Logger instance = null;
+        static readonly object LogMutex = new object();
+        private StreamWriter logFile;
+        private static bool enabled = true;
+
+        public static bool Enabled
+        {
+            get { return enabled; }
+            set { enabled = value; }
+        }
+
+        private Logger()
+        {
+            string logFilePath = "C:\\Scratch\\CesiumIonRevitAddin\\CesiumIonRevitAddinLog.txt";
+            logFile = new StreamWriter(logFilePath, true) { AutoFlush = true };
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (LogMutex)
+                    {
+                        if (instance == null) // Double-check locking
+                        {
+                            instance = new Logger();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            if (!enabled) return;
+
+            lock (LogMutex)
+            {
+                var now = DateTime.Now;
+                var timeStr = now.ToString("yyyy-MM-dd HH:mm:ss");
+                logFile.WriteLine($"{timeStr}: {message}");
+            }
+        }
+    }
+}
---
+++ CesiumIonRevitAddin/gltf/KHRTextureTransform.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using CesiumIonRevitAddin.Export;
+
+namespace CesiumIonRevitAddin.Gltf
+{
+    internal class KHRTextureTransform
+    {
+        [JsonProperty("offset")]
+        public XY? Offset { get; set; }
+        [JsonProperty("rotation")]
+        public double? Rotation { get; set; }
+        [JsonProperty("scale")]
+        public XY? Scale { get; set; }
+    }
+}
