5ef4a73bba579582638d61c6aa4c781533882a19
Ryan Veenstra <123468416+r-veenstra@users.noreply.github.com>
2024-11-08 08:23:13 +1000

    Update copyrights

--- CesiumIonRevitAddin/gltf/GltfVersion.cs
+++ CesiumIonRevitAddin/gltf/GltfVersion.cs
@@ -5,8 +5,8 @@ namespace CesiumIonRevitAddin.Gltf
     internal class GltfVersion
     {
         public string version = "2.0";
-        public string generator = "Cesium ion Revit generator";
-        public string copyright = "unsure";
+        public string generator = "Cesium ion for Autodesk Revit";
+        public string copyright = "Copyright 2024 Cesium GS, Inc.";
         public Dictionary<string, System.Object> extras = new Dictionary<string, System.Object>();
     }
 }
